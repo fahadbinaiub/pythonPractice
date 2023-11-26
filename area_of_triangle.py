f71caebfd420fb317812d8931b6f2884b2e72a44
fahadbinaiub <fbarafil7@gmail.com>
2023-11-26 11:54:02 +0600

    area of traingle

---
+++ area_of_triangle.py
@@ -0,0 +1,12 @@
+# area of a triangle
+num1 = int(input("Enter base: "))
+num2 = int(input("Enter height: "))
+
+result = .5 * num1 * num2
+
+print("The area of the given traingle:",result)
+
+num3 = float(input("Enter radius: "))
+
+result = 3.1416 * num3 ** 2
+print("The area of the circle is:",result)
